4a1316cd6b94ffaf90a6604fbd2cd4a1499084bc
Vladimir Ascencio <vladimirramos405@gmail.com>
2023-10-28 12:21:17 -0600

    Conexión a la BS

--- dfdfd/SistemaAdministrativo/FrmDatosInicio.Designer.cs
+++ dfdfd/SistemaAdministrativo/FrmDatosInicio.Designer.cs
@@ -29,252 +29,127 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDatosInicio));
-            this.pnlInicio = new System.Windows.Forms.Panel();
-            this.lblInicioFrmInicio = new System.Windows.Forms.Label();
-            this.pnlInformacionAd = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblTelefonoAd = new System.Windows.Forms.Label();
-            this.lblDepartamentoAd = new System.Windows.Forms.Label();
-            this.lblCorreoAd = new System.Windows.Forms.Label();
-            this.lblCarnetAd = new System.Windows.Forms.Label();
-            this.lblApellidoAd = new System.Windows.Forms.Label();
-            this.lblNombreAd = new System.Windows.Forms.Label();
-            this.pnlDadosAdmi = new System.Windows.Forms.Panel();
-            this.txtsShowNum = new System.Windows.Forms.TextBox();
-            this.txtShowDepart = new System.Windows.Forms.TextBox();
-            this.txtShowGmail = new System.Windows.Forms.TextBox();
-            this.txtShowCarnet = new System.Windows.Forms.TextBox();
-            this.txtShowLastName = new System.Windows.Forms.TextBox();
-            this.txtShowName = new System.Windows.Forms.TextBox();
-            this.pnlInicio.SuspendLayout();
-            this.pnlInformacionAd.SuspendLayout();
-            this.pnlDadosAdmi.SuspendLayout();
-            this.SuspendLayout();
+            pnlInicio = new Panel();
+            lblInicioFrmInicio = new Label();
+            pnlInformacionAd = new Panel();
+            label1 = new Label();
+            pnlDadosAdmi = new Panel();
+            gridDatosAd = new DataGridView();
+            pnlInicio.SuspendLayout();
+            pnlInformacionAd.SuspendLayout();
+            pnlDadosAdmi.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)gridDatosAd).BeginInit();
+            SuspendLayout();
             // 
             // pnlInicio
             // 
-            this.pnlInicio.Controls.Add(this.lblInicioFrmInicio);
-            this.pnlInicio.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlInicio.Location = new System.Drawing.Point(0, 0);
-            this.pnlInicio.Name = "pnlInicio";
-            this.pnlInicio.Size = new System.Drawing.Size(788, 32);
-            this.pnlInicio.TabIndex = 2;
+            pnlInicio.Controls.Add(lblInicioFrmInicio);
+            pnlInicio.Dock = DockStyle.Top;
+            pnlInicio.Location = new Point(0, 0);
+            pnlInicio.Margin = new Padding(4, 3, 4, 3);
+            pnlInicio.Name = "pnlInicio";
+            pnlInicio.Size = new Size(919, 37);
+            pnlInicio.TabIndex = 2;
             // 
             // lblInicioFrmInicio
             // 
-            this.lblInicioFrmInicio.AutoSize = true;
-            this.lblInicioFrmInicio.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblInicioFrmInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblInicioFrmInicio.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInicioFrmInicio.Location = new System.Drawing.Point(0, 0);
-            this.lblInicioFrmInicio.Name = "lblInicioFrmInicio";
-            this.lblInicioFrmInicio.Size = new System.Drawing.Size(109, 32);
-            this.lblInicioFrmInicio.TabIndex = 0;
-            this.lblInicioFrmInicio.Text = "INICIO";
+            lblInicioFrmInicio.AutoSize = true;
+            lblInicioFrmInicio.Dock = DockStyle.Top;
+            lblInicioFrmInicio.FlatStyle = FlatStyle.Flat;
+            lblInicioFrmInicio.Font = new Font("Times New Roman", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblInicioFrmInicio.Location = new Point(0, 0);
+            lblInicioFrmInicio.Margin = new Padding(4, 0, 4, 0);
+            lblInicioFrmInicio.Name = "lblInicioFrmInicio";
+            lblInicioFrmInicio.Size = new Size(109, 32);
+            lblInicioFrmInicio.TabIndex = 0;
+            lblInicioFrmInicio.Text = "INICIO";
             // 
             // pnlInformacionAd
             // 
-            this.pnlInformacionAd.AutoScroll = true;
-            this.pnlInformacionAd.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlInformacionAd.BackgroundImage")));
-            this.pnlInformacionAd.Controls.Add(this.label1);
-            this.pnlInformacionAd.Controls.Add(this.lblTelefonoAd);
-            this.pnlInformacionAd.Controls.Add(this.lblDepartamentoAd);
-            this.pnlInformacionAd.Controls.Add(this.lblCorreoAd);
-            this.pnlInformacionAd.Controls.Add(this.lblCarnetAd);
-            this.pnlInformacionAd.Controls.Add(this.lblApellidoAd);
-            this.pnlInformacionAd.Controls.Add(this.lblNombreAd);
-            this.pnlInformacionAd.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlInformacionAd.Location = new System.Drawing.Point(0, 32);
-            this.pnlInformacionAd.Name = "pnlInformacionAd";
-            this.pnlInformacionAd.Size = new System.Drawing.Size(200, 482);
-            this.pnlInformacionAd.TabIndex = 3;
+            pnlInformacionAd.AutoScroll = true;
+            pnlInformacionAd.BackgroundImage = (Image)resources.GetObject("pnlInformacionAd.BackgroundImage");
+            pnlInformacionAd.Controls.Add(label1);
+            pnlInformacionAd.Dock = DockStyle.Left;
+            pnlInformacionAd.Location = new Point(0, 37);
+            pnlInformacionAd.Margin = new Padding(4, 3, 4, 3);
+            pnlInformacionAd.Name = "pnlInformacionAd";
+            pnlInformacionAd.Size = new Size(233, 556);
+            pnlInformacionAd.TabIndex = 3;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 48);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "    DATOS DEL \r\nADMINISTRADOR  ";
-            // 
-            // lblTelefonoAd
-            // 
-            this.lblTelefonoAd.AutoSize = true;
-            this.lblTelefonoAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTelefonoAd.Location = new System.Drawing.Point(48, 432);
-            this.lblTelefonoAd.Name = "lblTelefonoAd";
-            this.lblTelefonoAd.Size = new System.Drawing.Size(91, 24);
-            this.lblTelefonoAd.TabIndex = 5;
-            this.lblTelefonoAd.Text = "Teléfono ";
-            // 
-            // lblDepartamentoAd
-            // 
-            this.lblDepartamentoAd.AutoSize = true;
-            this.lblDepartamentoAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDepartamentoAd.Location = new System.Drawing.Point(21, 368);
-            this.lblDepartamentoAd.Name = "lblDepartamentoAd";
-            this.lblDepartamentoAd.Size = new System.Drawing.Size(144, 24);
-            this.lblDepartamentoAd.TabIndex = 4;
-            this.lblDepartamentoAd.Text = "Departamento ";
-            // 
-            // lblCorreoAd
-            // 
-            this.lblCorreoAd.AutoSize = true;
-            this.lblCorreoAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCorreoAd.Location = new System.Drawing.Point(54, 320);
-            this.lblCorreoAd.Name = "lblCorreoAd";
-            this.lblCorreoAd.Size = new System.Drawing.Size(78, 24);
-            this.lblCorreoAd.TabIndex = 3;
-            this.lblCorreoAd.Text = "Correo ";
-            // 
-            // lblCarnetAd
-            // 
-            this.lblCarnetAd.AutoSize = true;
-            this.lblCarnetAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCarnetAd.Location = new System.Drawing.Point(55, 272);
-            this.lblCarnetAd.Name = "lblCarnetAd";
-            this.lblCarnetAd.Size = new System.Drawing.Size(77, 24);
-            this.lblCarnetAd.TabIndex = 2;
-            this.lblCarnetAd.Text = "Carnet ";
-            // 
-            // lblApellidoAd
-            // 
-            this.lblApellidoAd.AutoSize = true;
-            this.lblApellidoAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellidoAd.Location = new System.Drawing.Point(45, 216);
-            this.lblApellidoAd.Name = "lblApellidoAd";
-            this.lblApellidoAd.Size = new System.Drawing.Size(96, 24);
-            this.lblApellidoAd.TabIndex = 1;
-            this.lblApellidoAd.Text = "Apellidos ";
-            // 
-            // lblNombreAd
-            // 
-            this.lblNombreAd.AutoSize = true;
-            this.lblNombreAd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreAd.Location = new System.Drawing.Point(46, 160);
-            this.lblNombreAd.Name = "lblNombreAd";
-            this.lblNombreAd.Size = new System.Drawing.Size(95, 24);
-            this.lblNombreAd.TabIndex = 0;
-            this.lblNombreAd.Text = "Nombres ";
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(13, 250);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 48);
+            label1.TabIndex = 6;
+            label1.Text = "    DATOS DEL \r\nADMINISTRADOR  ";
             // 
             // pnlDadosAdmi
             // 
-            this.pnlDadosAdmi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlDadosAdmi.Controls.Add(this.txtsShowNum);
-            this.pnlDadosAdmi.Controls.Add(this.txtShowDepart);
-            this.pnlDadosAdmi.Controls.Add(this.txtShowGmail);
-            this.pnlDadosAdmi.Controls.Add(this.txtShowCarnet);
-            this.pnlDadosAdmi.Controls.Add(this.txtShowLastName);
-            this.pnlDadosAdmi.Controls.Add(this.txtShowName);
-            this.pnlDadosAdmi.Location = new System.Drawing.Point(200, 152);
-            this.pnlDadosAdmi.Name = "pnlDadosAdmi";
-            this.pnlDadosAdmi.Size = new System.Drawing.Size(589, 360);
-            this.pnlDadosAdmi.TabIndex = 4;
+            pnlDadosAdmi.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlDadosAdmi.Controls.Add(gridDatosAd);
+            pnlDadosAdmi.Location = new Point(233, 175);
+            pnlDadosAdmi.Margin = new Padding(4, 3, 4, 3);
+            pnlDadosAdmi.Name = "pnlDadosAdmi";
+            pnlDadosAdmi.Size = new Size(687, 415);
+            pnlDadosAdmi.TabIndex = 4;
             // 
-            // txtsShowNum
+            // gridDatosAd
             // 
-            this.txtsShowNum.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtsShowNum.Location = new System.Drawing.Point(48, 304);
-            this.txtsShowNum.Multiline = true;
-            this.txtsShowNum.Name = "txtsShowNum";
-            this.txtsShowNum.ReadOnly = true;
-            this.txtsShowNum.Size = new System.Drawing.Size(208, 32);
-            this.txtsShowNum.TabIndex = 5;
-            // 
-            // txtShowDepart
-            // 
-            this.txtShowDepart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtShowDepart.Location = new System.Drawing.Point(48, 240);
-            this.txtShowDepart.Multiline = true;
-            this.txtShowDepart.Name = "txtShowDepart";
-            this.txtShowDepart.ReadOnly = true;
-            this.txtShowDepart.Size = new System.Drawing.Size(440, 32);
-            this.txtShowDepart.TabIndex = 4;
-            // 
-            // txtShowGmail
-            // 
-            this.txtShowGmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtShowGmail.Location = new System.Drawing.Point(48, 184);
-            this.txtShowGmail.Multiline = true;
-            this.txtShowGmail.Name = "txtShowGmail";
-            this.txtShowGmail.ReadOnly = true;
-            this.txtShowGmail.Size = new System.Drawing.Size(440, 32);
-            this.txtShowGmail.TabIndex = 3;
-            // 
-            // txtShowCarnet
-            // 
-            this.txtShowCarnet.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtShowCarnet.Location = new System.Drawing.Point(48, 136);
-            this.txtShowCarnet.Multiline = true;
-            this.txtShowCarnet.Name = "txtShowCarnet";
-            this.txtShowCarnet.ReadOnly = true;
-            this.txtShowCarnet.Size = new System.Drawing.Size(192, 32);
-            this.txtShowCarnet.TabIndex = 2;
-            // 
-            // txtShowLastName
-            // 
-            this.txtShowLastName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtShowLastName.Location = new System.Drawing.Point(48, 88);
-            this.txtShowLastName.Multiline = true;
-            this.txtShowLastName.Name = "txtShowLastName";
-            this.txtShowLastName.ReadOnly = true;
-            this.txtShowLastName.Size = new System.Drawing.Size(440, 32);
-            this.txtShowLastName.TabIndex = 1;
-            // 
-            // txtShowName
-            // 
-            this.txtShowName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtShowName.Location = new System.Drawing.Point(48, 40);
-            this.txtShowName.Multiline = true;
-            this.txtShowName.Name = "txtShowName";
-            this.txtShowName.ReadOnly = true;
-            this.txtShowName.Size = new System.Drawing.Size(440, 32);
-            this.txtShowName.TabIndex = 0;
+            gridDatosAd.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridDatosAd.Dock = DockStyle.Fill;
+            gridDatosAd.Location = new Point(0, 0);
+            gridDatosAd.Name = "gridDatosAd";
+            gridDatosAd.ReadOnly = true;
+            gridDatosAd.RowTemplate.Height = 25;
+            gridDatosAd.Size = new Size(687, 415);
+            gridDatosAd.TabIndex = 0;
+            gridDatosAd.CellContentClick += gridDatosAd_CellContentClick;
             // 
             // FrmDatosInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(788, 514);
-            this.Controls.Add(this.pnlDadosAdmi);
-            this.Controls.Add(this.pnlInformacionAd);
-            this.Controls.Add(this.pnlInicio);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "FrmDatosInicio";
-            this.Text = "FrmDatosInicio";
-            this.pnlInicio.ResumeLayout(false);
-            this.pnlInicio.PerformLayout();
-            this.pnlInformacionAd.ResumeLayout(false);
-            this.pnlInformacionAd.PerformLayout();
-            this.pnlDadosAdmi.ResumeLayout(false);
-            this.pnlDadosAdmi.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(919, 593);
+            Controls.Add(pnlDadosAdmi);
+            Controls.Add(pnlInformacionAd);
+            Controls.Add(pnlInicio);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "FrmDatosInicio";
+            Text = "FrmDatosInicio";
+            pnlInicio.ResumeLayout(false);
+            pnlInicio.PerformLayout();
+            pnlInformacionAd.ResumeLayout(false);
+            pnlInformacionAd.PerformLayout();
+            pnlDadosAdmi.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)gridDatosAd).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pnlInicio;
-        private System.Windows.Forms.Label lblInicioFrmInicio;
-        private System.Windows.Forms.Panel pnlInformacionAd;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblTelefonoAd;
-        private System.Windows.Forms.Label lblDepartamentoAd;
-        private System.Windows.Forms.Label lblCorreoAd;
-        private System.Windows.Forms.Label lblCarnetAd;
-        private System.Windows.Forms.Label lblApellidoAd;
-        private System.Windows.Forms.Label lblNombreAd;
-        private System.Windows.Forms.Panel pnlDadosAdmi;
-        private System.Windows.Forms.TextBox txtsShowNum;
-        private System.Windows.Forms.TextBox txtShowDepart;
-        private System.Windows.Forms.TextBox txtShowGmail;
-        private System.Windows.Forms.TextBox txtShowCarnet;
-        private System.Windows.Forms.TextBox txtShowLastName;
-        private System.Windows.Forms.TextBox txtShowName;
+        private Panel pnlInicio;
+        private Label lblInicioFrmInicio;
+        private Panel pnlInformacionAd;
+        private Label label1;
+        private Label lblTelefonoAd;
+        private Label lblDepartamentoAd;
+        private Label lblCorreoAd;
+        private Label lblCarnetAd;
+        private Label lblApellidoAd;
+        private Label lblNombreAd;
+        private Panel pnlDadosAdmi;
+        private TextBox txtsShowNum;
+        private TextBox txtShowDepart;
+        private TextBox txtShowGmail;
+        private TextBox txtShowCarnet;
+        private TextBox txtShowLastName;
+        private TextBox txtShowName;
+        private DataGridView gridDatosAd;
     }
 }
--- dfdfd/SistemaAdministrativo/FrmDatosInicio.cs
+++ dfdfd/SistemaAdministrativo/FrmDatosInicio.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace SistemaAdministrativo
 {
@@ -15,6 +18,33 @@ namespace SistemaAdministrativo
         public FrmDatosInicio()
         {
             InitializeComponent();
+            CargarDatosEnDataGridViem();
+        }
+
+        private void CargarDatosEnDataGridViem()
+        {
+            string cadenaConexion = "Data Source=DESKTOP-E4D98NB\\SQLEXPRESS;Initial Catalog=proyectoSocial;Integrated Security=True";
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                connection.Open();
+
+                string consulta = "SELECT * FROM dbo.DatosAlumnos";
+
+                using (SqlCommand command = new SqlCommand(consulta, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+                }
+            }
+
+            gridDatosAd.DataSource = dataTable;
+        }
+
+        private void gridDatosAd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Puedes manejar eventos del DataGridView si es necesario
         }
     }
 }
