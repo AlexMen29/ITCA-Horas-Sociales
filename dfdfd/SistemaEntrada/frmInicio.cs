1f2edf13ad1f774bc2e94d26b0bdcf87f9d98e89
Mushrooms <fernandobermudez1606@gmail.com>
2023-11-16 21:43:46 -0600

    Funcion de Validacion Numerica creada

--- dfdfd/SistemaEntrada/frmInicio.cs
+++ dfdfd/SistemaEntrada/frmInicio.cs
@@ -121,7 +121,16 @@ namespace Login
     {
         public static string carnetIngresado { get; set; }
         public static int Nivelusuario { get; set; }
-
         public static DatosAlumno usuario { get; set; }
+        public static void ValidacionNumerica(object sender, KeyPressEventArgs e)
+        {
+            // Verifica si la tecla presionada no es un número
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            { 
+                MessageBox.Show("Escriba un NÚMERO válido", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Handled = true; // Indica que la tecla fue manejada y no se procesará más
+            }
+        }
+
     }
 }
